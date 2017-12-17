5a3d5c3ca1968569cc9e2f3f9a99f503d639dd6e
hongfz <hongfz16@163.com>
2017-12-17 11:50:23 +0800

    fix bugs that crash on 2000 html samples

---
+++ DataStructrue/Experiment(1)/Experiment(1)V2.0/Experiment1/Experiment1/CharString.h
@@ -0,0 +1,131 @@
+﻿/**
+@brief 声明CharString字符串类
+@file CharString.h
+@author 洪方舟
+@email hongfz16@163.com
+@version 1.0
+@date 2017.11.3
+*/
+
+#ifndef _CHARSTRING_H_
+#define _CHARSTRING_H_
+
+#include <iostream>
+#include <string>
+using std::string;
+
+class CharString
+{
+public:
+
+	/**********构造函数、析构函数**********/
+
+	//默认构造函数，预留1000个位置
+	CharString();
+	
+	//重载构造函数1，构造出包含一个给定字符的字符串
+	CharString(char init);
+	
+	//重载构造函数2，构造出包含num个给定字符的字符串
+	CharString(char init,int num);
+	
+	//重载构造函数3，使用string类型初始化
+	CharString(string& init);
+	
+	//重载构造函数4，使用char*初始化
+	CharString(char* init);
+	
+	
+	//拷贝构造函数
+	CharString(const CharString& cs);
+	
+	//析构函数，释放base指向的空间
+	~CharString();
+
+	/**********文档要求实现的函数**********/
+	
+	//返回从pos开始的第一个匹配targetString的位置的index（pos从0开始数，以下表示index的都是从0开始数）
+	int indexOf(CharString& targetString,int pos);
+	
+	//返回从pos位置开始长度为len的字符串（包含第pos个）
+	CharString subString(int pos,int len);
+
+	//将targetString连接到本身之后，返回一个连接好的字符串的指针，这个操作并不会改变本身或者targetString
+	CharString* concat(CharString& targetString);
+
+	//赋值，将targetString赋值给this
+	void assign(CharString& targetString);
+
+
+	/**********其他函数**********/
+
+	//将c加入到本字符串末尾
+	void append(char c);
+
+	//将c字符串接到本字符串末尾
+	void append(char* c);
+
+	//将string接到本字符串末尾
+	void append(string& s);
+
+	//将CharString字符串加到本字符串末尾
+	void append(CharString& cs);
+
+	//返回第index个字符
+	char getIndex(int index) const;
+	
+	//返回字符串长度
+	int getSize() const;
+
+	//返回字符串预留长度
+	int getCapability() const;
+	
+	//打印字符串
+	void print(std::ostream& os);
+	
+	//如果字符串为空就返回true，不为空返回false
+	bool empty();
+	
+	//清空字符串
+	void clear();
+
+	//如果两字符串相同返回true，否则返回false
+	bool is_equal(CharString target) const;
+
+	//上一个函数的string重载版本
+	bool is_equal(string target);
+
+	//重载标准输出流操作符
+	friend std::ostream& operator<<(std::ostream& out,CharString& obj);
+	
+	//赋值函数
+	CharString& operator=(const CharString& other);
+
+	//重载相等运算符
+	bool operator == (const CharString& other) const;
+
+	//重载小于运算符
+	bool operator < (const CharString& other) const;
+
+	//输出字符串
+	void print_CharString(std::ostream& os);
+
+private:
+	//未指定长度时的默认长度
+	static int init_size;
+
+	//每次需要扩展长度的时候增加的size
+	static int delta_size;
+
+	//预留的空间
+	int size;
+	
+	//字符串长度
+	int num;
+	
+	//指向字符串的指针
+	char* base;
+
+};
+
+#endif
---
+++ DataStructrue/Experiment(1)/Experiment(1)V2.0/Experiment1/Experiment1/extractKeyWords.h
@@ -0,0 +1,64 @@
+﻿/**
+@brief 栈结构网页解析函数声明
+@file extractKeyWords.h
+@author 洪方舟
+@email hongfz16@163.com
+@version 1.0
+@date 2017.11.3
+*/
+
+#ifndef _EXTRACT_KEY_WORDS_
+#define _EXTRACK_KEY_WORDS_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+#include "UnicodeToGBK.h"
+#include "wordSegmentation.h"
+#include "Stack.h"
+#include "CharString.h"
+#include "Link.h"
+using namespace std;
+
+//自定义结构用于存放html标签信息
+class label;
+
+//传入整个文本的引用与当前位置，返回标签信息
+label get_label_info(CharString& html,int pos);
+
+//传入整个文本的引用与当前位置，返回闭合标签信息
+CharString get_close_info(CharString& html,int pos);
+
+//提取发帖大类，发帖小类，发帖标题，返回值为查找结束后所在位置
+int find_basic_info(Stack<label>& m_stack,CharString& html,int pos,CharString* info);
+
+//提取发帖内容，返回值为查找结束后所在位置
+int find_content(Stack<label>& m_stack,CharString& html,int pos,CharString* info);
+
+//提取发帖人，返回值为查找结束后所在位置
+int find_author(Stack<label>& m_stack,CharString& html,int pos,CharString* info);
+
+//提取发帖日期，返回值为查找结束后所在位置
+int find_date(Stack<label>& m_stack,CharString& html,int pos,CharString* info);
+
+//提取发帖类型，返回值为查找结束后所在位置
+int find_type(Stack<label>& m_stack,CharString& html,int pos,CharString* info);
+
+//栈结构遍历html文本内容总函数，负责调用上面的各个函数
+void traverse_html(Stack<label>& m_stack,CharString& html,CharString* info);
+
+//提取信息结束后将info内的信息以csv格式输出，并实现部分网页内容的转码，以及将分词结果输出
+void print_info(CharString* info,int _size,ofstream& os,wordSegmentation& dic,UnicodeToGBK& u2g);
+
+//初始化html文本内容，调用traverse_html函数
+void extractInfo(CharString* info,string filename);
+
+//传入html文件名，输出文件名，开始解析网页内容
+void extractKeyWords(string inputfilename,string outputfilename,string url,int count,wordSegmentation& dic,UnicodeToGBK& u2g);
+
+
+
+
+#endif //_EXTRACK_KEY_WORDS_
